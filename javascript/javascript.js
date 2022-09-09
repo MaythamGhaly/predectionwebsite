f307fb9eca69c230496099fb0d2c735e6b928c7e
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-10 00:57:45 +0300

    add-fitch-for-age

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,6 @@
 window.onload = () => {
-    const list=document.querySelector("list")
+  var arr=[]
+    const list=document.getElementById("list")
     const btn=document.getElementById("submit")
     btn.addEventListener("click",getdata)
     function getdata(){
@@ -7,12 +8,24 @@ window.onload = () => {
       fetch(`https://api.genderize.io?name=${name}`)
         .then(res=>res.json())
         .then(data=> {
-          console.log(data.gender)
-          const list1=document.createElement("li")
-          const gender=document.createTextNode(data.gender)
-       list1.appendChild=('gender')
+          arr.push(data.gender)
+          console.log(arr[0])
         })
-        
+        fetch(`https://api.agify.io/?name=${name}`)
+        .then(res=>res.json())
+        .then(data=> {
+          arr.push(data.age)
+          console.log(arr[1])
+        })
+        fetch(`https://api.nationalize.io/?name=${name}`)
+        .then(res=>res.json())
+        .then(data=> {
+          data.forEach(fuction => {
+            arr.push(data.country_id)
+          });
+          
+          console.log(arr[2])
+        })
         
 
     
