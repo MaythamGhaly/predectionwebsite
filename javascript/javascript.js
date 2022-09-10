b5f1022520aab197a1da29be4dbbded2c254b4c6
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-10 13:48:10 +0300

    fix the html and css files

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,6 +3,12 @@ window.onload = () => {
     const list=document.getElementById("list")
     const btn=document.getElementById("submit")
     btn.addEventListener("click",getdata)
+
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(res=>res.json())
+        .then(data=> {
+          img=document.getElementById("dogimg").innerHTML=`<img class="img" src="${data.message}"/>`
+          });
     function getdata(){
       var name=document.getElementById("name").value
       fetch(`https://api.genderize.io?name=${name}`)
@@ -25,12 +31,6 @@ window.onload = () => {
           
           const gender=document.getElementById("nationality").innerHTML=(data.country[0].country_id +" / "+ data.country[1].country_id)
           });
-        fetch(`https://dog.ceo/api/breeds/image/random`)
-        .then(res=>res.json())
-        .then(data=> {
-          document.getElementById("dogimg").innerHTML=`<img src="${data.message}"/>`
-          });
-
         document.getElementById("info").style.display="block"
           
           
