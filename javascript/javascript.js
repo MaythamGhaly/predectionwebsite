fb2d53af723c5c53698477c38e54362b89e04fcc
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-10 14:16:00 +0300

    clean and comment code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,44 +1,31 @@
 window.onload = () => {
-  var arr=[]
-    const list=document.getElementById("list")
     const btn=document.getElementById("submit")
     btn.addEventListener("click",getdata)
-
+    // fetch function for the image
     fetch(`https://dog.ceo/api/breeds/image/random`)
         .then(res=>res.json())
         .then(data=> {
           img=document.getElementById("dogimg").innerHTML=`<img class="img" src="${data.message}"/>`
           });
+    // function for fetch the gender,age, and nationality
     function getdata(){
       var name=document.getElementById("name").value
       fetch(`https://api.genderize.io?name=${name}`)
         .then(res=>res.json())
         .then(data=> {
-          arr.push(data.gender)
           const gender=document.getElementById("gender").innerHTML=data.gender
         })
         fetch(`https://api.agify.io/?name=${name}`)
         .then(res=>res.json())
         .then(data=> {
-          arr.push(data.age)
           const gender=document.getElementById("age").innerHTML=data.age
         })
         fetch(`https://api.nationalize.io/?name=${name}`)
         .then(res=>res.json())
         .then(data=> {
-          arr.push(data.country[0].country_id)
-          arr.push(data.country[1].country_id)
-          
           const gender=document.getElementById("nationality").innerHTML=(data.country[0].country_id +" / "+ data.country[1].country_id)
           });
         document.getElementById("info").style.display="block"
-          
-          
-      
-        
-
-    
     }
-    
-  }
+}
   
